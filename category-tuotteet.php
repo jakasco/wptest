8dbbe84aa6d83546bfb4c73ea8ae9ce2e76e6a6d
Jakasco <kasperi.mutku@elisanet.fi>
2020-05-27 18:17:42 +0300

    tuotteet sivu categoriat valmis

--- category-tuotteet.php
+++ category-tuotteet.php
@@ -1,4 +1,3 @@
-
 <?php
 get_header(); ?>
 <div class="content-row">
@@ -7,47 +6,20 @@ get_sidebar();
 ?>
 
 <main>
-	<h2><?php echo get_queried_object()->name; ?></h2>
-	<p> <?php echo get_queried_object()->description; ?></p>
- <h3 class="sininen">kategoriat</h3>
- <?php
-	$id = get_queried_object()->term_id;
-	$tyyppi = get_queried_object()->taxonomy;
-	$alikategoriat = get_term_children( $id, $tyyppi ); //haetaan alikategoriat
-	foreach( $alikategoriat as $alikategoria):
-		$termi = get_term_by('id',$alikategoria, $tyyppi);
- ?>
- <article>
-	<a href="<?php echo get_term_link($alikategoria, $tyyppi ); ?>">
+</h1>TUOTTEET </h1>
 	<?php
-	$artikkelit = get_posts( array('category' => $termi->term_id, 'numberposts' => 1, 'orderby' => 'rand'));
-	
-	$thumbnail = get_the_post_thumbnail( $artikkelit[0]->ID, 'thumbnail');
-	
-	if($thumbnail == null){
-		?> <img src="http://placekitten.com/150/200" alt="">
-	<?php	
-	}else{
-		echo $thumbnail;
-	}
+	    global $post;
+	    $args = array( 'numberposts' => 10, 'category_name' => 'ad' );
+	    $posts = get_posts( $args );
+	    foreach( $posts as $post ): setup_postdata($post);
 	?>
 
-		<h4><?php echo $termi->nimi; ?></h4>
-		<p>
-			<?php 
-			
-			$description = substr(category_description( $termi->term_id ), 0, 100); 
-			
-			if($description == ""){
-				echo "Ei kategoria kuvausta";
-			}else{
-			echo $description;
-			}
-			?>
-		</p>
-	</a>
- </article>
- <?php endforeach;?>
+	<div >
+		<h3>  <?php the_title(); ?> </h3>
+	</div>
+
+	<?php endforeach; ?>
+
  </main>
 </div>
 
--- productsTemplate.php
+++ productsTemplate.php
@@ -5,30 +5,73 @@
 <div id="primary" class="content-area">
 
                 <main id="main" class="site-main" role="main">
-                </h1>Tuotteet</h1>
 
-                <?php
-$category_query_args = array(
-    'tuotteet' => $tuotteet
-);
-//echo implode(" ",$category_query_args);
-$category_query = new WP_Query( $category_query_args );
 
- if ($category_query->have_posts()):?>
+                  </h1>TUOTTEET </h1>
+                  <?php
+                  $this_category = get_term_by( 'slug', 'ad');//get_category("tuotteet");
+                      //echo $this_category->cat_ID;
+                      $parent_term_id =$this_category->cat_ID; // term id of parent term
 
- <?php
-            global $post;
-            $args = array( 'category' => 'tuotteet' );
-            $myposts = get_posts( $args );
-            foreach( $myposts as $post ) :  setup_postdata($post); ?>
-              <div><h3><?php echo the_title(); ?></h3>
-                <li class="testimonial"><?php the_content(); ?></li><br/>
-              </div>
-            <?php endforeach;
+                  //$termchildren = get_terms('category',array('child_of' => $parent_id));
+                  $taxonomies = array(
+                      'taxonomy' => 'category'
+                  );
 
- endif;
-             ?>
-        </ul>
+                  $args = array(
+                     // 'parent'         => $parent_term_id,
+                       'child_of'      => $parent_term_id
+                  );
+
+                  $terms = get_terms($taxonomies, $args);
+                  if (sizeof($terms)>0)
+                  {
+
+                  echo ' <div class="categories">  ';
+                  echo '<p> Sub Categories of '. get_cat_name( $parent_term_id ) .'</p>';
+
+  $count = 0;
+                  foreach ( $terms as $term ) {
+echo '<ul>';
+              /*       $term_link = sprintf( '<div class="custom-cats"><a href="%1$s" alt="%2$s">%3$s</a></div>',
+                     esc_url(
+                        get_category_link( $term->term_id ) ),
+                          esc_attr( sprintf( 'View all posts in %s', 'textdomain' ), $term->name ),
+                          esc_html( $term->name ));
+
+                      echo sprintf( $term_link );
+                    }*/
+
+
+                  $count++;
+                  if ($count <= 3) { //näytä 3 alakategoriaa
+                    echo    '<h3>count of posts : '. $term->count . " / "  . $term->name . '</h3>';
+
+                    global $post2;
+
+                    $args4 = array( 'numberposts' => 10, 'category_name' => $term->slug );
+                    $posts2 = get_posts( $args4 );
+
+
+                    foreach( $posts2 as $post ): setup_postdata($post);
+                    $title = the_title();
+echo "<li>count: " . $args4['category_name'] . "</li>";
+echo '</ul>'
+?>
+
+<?php
+
+                    endforeach;
+                //       echo $args2['category_name'];
+                    } //if count
+
+
+
+}//foreach
+
+                  echo '</div><!-- categories div end-->';
+                      }
+                   ?>
 
                 </main><!-- .site-main -->
                 <?php get_sidebar( 'content-bottom' ); ?>
